42d31ef91e3110aa37c2f76ec3249e7af4103f92
Haseeb Arain <haseebarain105@gmail.com>
2025-04-26 19:18:04 +0500

    Update Therapist.java

--- Therapist.java
+++ Therapist.java
@@ -3,8 +3,6 @@ public class Therapist extends User {
     public Therapist(String name, String email) {
         super(name, email);
     }
-
-    @Override
     void displayInfo() {
         System.out.println("Therapist: " + name);
     }
